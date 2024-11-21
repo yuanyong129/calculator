69daf4b42972e7d058dacf42189663d51e5b86a7
yuanyong <yuanyong129@outlook.com>
2024-11-21 17:27:41 +0800

    feat(标准): 键盘按钮

--- src/app/routes/calculators/standard/keyboard-area/keyboard-area.component.ts
+++ src/app/routes/calculators/standard/keyboard-area/keyboard-area.component.ts
@@ -1,16 +1,17 @@
 import { Component } from "@angular/core";
-import { MATH_SYMBOL } from "@share/constant";
-import { MatCardModule } from "@angular/material/card";
+import { MATH_SYMBOL } from "@/app/shared/constant";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
 
 interface IKeyItem {
-  label: string;
+  label?: string;
   value: string;
   isIcon: boolean;
   iconName?: string;
 }
 
 @Component({
-  imports: [MatCardModule],
+  imports: [MatButtonModule, MatIconModule],
   selector: 'keyboard-area',
   standalone: true,
   templateUrl: './keyboard-area.component.html',
@@ -25,29 +26,34 @@ interface IKeyItem {
 })
 export class KeyboardAreaComponent {
   keyItems: IKeyItem[] = [
-    { label: '%', value: MATH_SYMBOL.PERCENT, isIcon: false },
-    { label: '%', value: MATH_SYMBOL.PERCENT, isIcon: false },
-    { label: '%', value: MATH_SYMBOL.PERCENT, isIcon: false },
-    { label: '%', value: MATH_SYMBOL.PERCENT, isIcon: false },
-    { label: '%', value: MATH_SYMBOL.PERCENT, isIcon: false },
-    { label: '%', value: MATH_SYMBOL.PERCENT, isIcon: false },
-    { label: '%', value: MATH_SYMBOL.PERCENT, isIcon: false },
-    { label: '%', value: MATH_SYMBOL.PERCENT, isIcon: false },
-    { label: '%', value: MATH_SYMBOL.PERCENT, isIcon: false },
-    { label: '%', value: MATH_SYMBOL.PERCENT, isIcon: false },
-    { label: '%', value: MATH_SYMBOL.PERCENT, isIcon: false },
-    { label: '%', value: MATH_SYMBOL.PERCENT, isIcon: false },
-    { label: '%', value: MATH_SYMBOL.PERCENT, isIcon: false },
-    { label: '%', value: MATH_SYMBOL.PERCENT, isIcon: false },
-    { label: '%', value: MATH_SYMBOL.PERCENT, isIcon: false },
-    { label: '%', value: MATH_SYMBOL.PERCENT, isIcon: false },
-    { label: '%', value: MATH_SYMBOL.PERCENT, isIcon: false },
-    { label: '%', value: MATH_SYMBOL.PERCENT, isIcon: false },
-    { label: '%', value: MATH_SYMBOL.PERCENT, isIcon: false },
-    { label: '%', value: MATH_SYMBOL.PERCENT, isIcon: false },
-    { label: '%', value: MATH_SYMBOL.PERCENT, isIcon: false },
-    { label: '%', value: MATH_SYMBOL.PERCENT, isIcon: false },
-    { label: '%', value: MATH_SYMBOL.PERCENT, isIcon: false },
-    { label: '%', value: MATH_SYMBOL.PERCENT, isIcon: false },
+    { value: MATH_SYMBOL.PERCENT, isIcon: true, iconName: 'percent-outline' },
+    { label: 'CE', value: MATH_SYMBOL.CLEAR_ERROR, isIcon: false },
+    { label: 'C', value: MATH_SYMBOL.CLEAR, isIcon: false },
+    { value: MATH_SYMBOL.PERCENT, isIcon: true, iconName: 'backspace-outline' },
+
+    { value: MATH_SYMBOL.RECIPROCAL, label: MATH_SYMBOL.RECIPROCAL, isIcon: false },
+    { value: MATH_SYMBOL.SQUARE, label: MATH_SYMBOL.SQUARE, isIcon: false },
+    { value: MATH_SYMBOL.SQUARE_ROOT, isIcon: true, iconName: 'square-root' },
+    { value: MATH_SYMBOL.DIVIDE, isIcon: true, iconName: 'division' },
+
+    { value: '7', label: '7', isIcon: false },
+    { value: '8', label: '8', isIcon: false },
+    { value: '9', label: '9', isIcon: false },
+    { value: MATH_SYMBOL.MULTIPLE, isIcon: true, iconName: 'close' },
+
+    { value: '4', label: '4', isIcon: false },
+    { value: '5', label: '5', isIcon: false },
+    { value: '6', label: '6', isIcon: false },
+    { value: MATH_SYMBOL.MINUS, isIcon: true, iconName: 'minus' },
+
+    { value: '1', label: '1', isIcon: false },
+    { value: '2', label: '2', isIcon: false },
+    { value: '3', label: '3', isIcon: false },
+    { value: MATH_SYMBOL.PLUS, isIcon: true, iconName: 'plus' },
+
+    { value: MATH_SYMBOL.NEGATE, isIcon: true, iconName: 'plus-minus-variant' },
+    { value: '0', label: '0', isIcon: false },
+    { value: MATH_SYMBOL.POINT, label: MATH_SYMBOL.POINT, isIcon: false },
+    { value: MATH_SYMBOL.EQUAL, isIcon: true, iconName: 'equal' },
   ];
 }
